332c99b887932b89f92a23986746aecc97606d3c
santiagoTrivi <santiagocarvajal103@gmail.com>
2024-07-17 03:12:33 -0400

    feat(product): edit and create product modals

--- resources/views/admin/product/manager.blade.php
+++ resources/views/admin/product/manager.blade.php
@@ -9,30 +9,8 @@
 @section('content')
 
 <div>
-    <div class="card card-primary card-outline">
-        <div class="card-header">
-            <div class="row no-print">
-                <div class="col-12">
-                    <div>
-                        <h3 class="card-title">{{Auth::user()->name}}</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="card-body table-responsive p-0">
-            <table id="product-content" class="table table-hover text-nowrap" style="cursor:pointer">
-                <thead>
-                    <tr>
-                        <th>Fecha de registro</th>
-                        <th>Nombre</th>
-                        <th>Comercializadoras</th>
-                    </tr>
-                </thead>
+    <livewire:product-manager />
 
-            </table>
-        </div>
-
-    </div>
 </div>
 
 
@@ -49,60 +27,6 @@
 
 @section('js')
 <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
-<script src="https://cdn.datatables.net/2.0.8/js/dataTables.bootstrap5.js"></script>
 
-<script src="https://cdn.datatables.net/buttons/3.0.2/js/dataTables.buttons.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.dataTables.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.print.min.js"></script>
-
-<script>
-    const table = new DataTable('#product-content', {
-        dom: 'Bfrtip',
-        buttons: [
-            {
-                extend: 'excelHtml5',
-                title: `Gestión de productos - ${new Date()}`
-            }
-        ],
-        "processing": true,
-        "serverSide": true,
-        "ajax": {
-            "url": "{{route('api.product.query')}}",
-            "type": "GET",
-            "data": {
-            }
-        },
-        "language": {
-            "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
-        },
-        "columns": [
-            { data: 'created_at' },
-            { data: 'product_name' },
-            { data: 'company_name' }
-
-        ],
-        "columnDefs": [
-            { className: "dt-head-center", targets: [0, 1, 2] },
-            { className: "text-capitalize", targets: [0, 1, 2] }
-        ],
-        "order": [
-            [0, "desc"]
-        ],
-    });
-
-
-    $('#company-content').on('click', 'tbody tr', function () {
-        const row = table.row(this).data();
-        console.log(row);
-        window.location.href = "{{ route('admin.company.manager.details', ':id') }}".replace(':id', row.id);
-    })
-
-</script>
 
 @stop
