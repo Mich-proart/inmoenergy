515d704520ead361ecf8fe9c2f430e267b4ab0ca
santiagoTrivi <santiagocarvajal103@gmail.com>
2025-03-22 22:17:07 -0400

    update: side bar style changed

--- config/adminlte.php
+++ config/adminlte.php
@@ -64,7 +64,7 @@ return [
     */
 
     'logo' => '<b>Lenders Consulting</b>',
-    'logo_img' => 'vendor/adminlte/dist/img/Logo_sinnombre-removebg.png',
+    'logo_img' => 'vendor/adminlte/dist/img/Logo_sinnombre_resize_large.jpg',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
