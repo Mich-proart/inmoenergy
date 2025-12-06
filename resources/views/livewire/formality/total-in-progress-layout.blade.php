fc2d9c62e341b649b6e496b00b182bfa8e93cd0d
santiagoTrivi <santiagocarvajal103@gmail.com>
2025-12-05 22:06:22 -0400

    fix(chapter 2): especific displayname on filter

--- resources/views/livewire/formality/total-in-progress-layout.blade.php
+++ resources/views/livewire/formality/total-in-progress-layout.blade.php
@@ -57,6 +57,7 @@
                                         <div class="dropdown-item">
                                             <input wire:model.live="selectedUsers" wire:change="isAllCheckUsers"
                                                 type="checkbox" value="{{ $user->id }}"> {{ $user->name }}
+                                            {{ $user->first_last_name }} {{ $user->second_last_name }}
                                         </div>
                                     </li>
                                 @endforeach
@@ -225,6 +226,7 @@
                                         <div class="dropdown-item">
                                             <input wire:model.live="selectedIssuers" wire:change="isAllCheckIssuers"
                                                 type="checkbox" value="{{ $issuer->id }}"> {{ $issuer->name }}
+                                            {{ $issuer->first_last_name }} {{ $issuer->second_last_name }}
                                         </div>
                                     </li>
                                 @endforeach
